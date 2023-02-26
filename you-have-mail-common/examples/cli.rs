f4c51cdc55688b0f82f062239d4afcbfa91e736b
Leander Beernaert <lbb-dev@pm.me>
2023-02-26 19:26:56 +0100

    refactor: Update CLI example
    CLI example will now attempt to store the observer state and load it on
    the next run.

--- you-have-mail-common/examples/cli.rs
+++ you-have-mail-common/examples/cli.rs
@@ -1,10 +1,14 @@
 use proton_api_rs::tokio;
 use proton_api_rs::tokio::io::AsyncBufReadExt;
 use proton_api_rs::tokio::io::AsyncWriteExt;
+use secrecy::{ExposeSecret, Secret};
+use std::path::PathBuf;
 use std::sync::Arc;
 use std::time::Duration;
 use you_have_mail_common::backend::Backend;
-use you_have_mail_common::{Account, AccountError, Notifier, ObserverBuilder};
+use you_have_mail_common::{
+    Account, AccountError, Config, ConfigAccount, EncryptionKey, Notifier, ObserverBuilder,
+};
 
 #[cfg(feature = "proton-backend")]
 fn new_backed() -> Arc<dyn Backend> {
@@ -39,39 +43,125 @@ impl Notifier for StdOutNotifier {
 
 #[tokio::main(worker_threads = 1)]
 async fn main() {
-    let email = std::env::var("YHM_EMAIL").unwrap();
-    let password = std::env::var("YHM_PASSWORD").unwrap();
-
+    let encryption_key = get_or_create_encryption_key();
     let backend = new_backed();
-    let mut account = Account::new(backend, &email);
-    account.login(&password).await.unwrap();
+    let accounts = if let Some(accounts) =
+        load_config(encryption_key.expose_secret(), &[backend.clone()]).await
+    {
+        println!("Previous accounts detected");
+        let mut result = Vec::with_capacity(accounts.len());
+        for (mut a, refresher) in accounts {
+            if let Some(r) = refresher {
+                println!("Refreshing account: {}", a.email());
+                a.refresh(r).await.unwrap();
+            } else {
+                println!("Account {} is logged out", a.email());
+            }
+            result.push(a);
+        }
+        result
+    } else {
+        println!("No previous accounts logging in with ENV{{YHM_EMAIL}} and ENV{{YHM_PASSWORD}}");
+        let email = std::env::var("YHM_EMAIL").unwrap();
+        let password = std::env::var("YHM_PASSWORD").unwrap();
 
-    if account.is_awaiting_totp() {
-        let mut stdout = tokio::io::stdout();
-        let mut line_reader = tokio::io::BufReader::new(tokio::io::stdin()).lines();
-        stdout
-            .write_all("Please Input TOTP:".as_bytes())
-            .await
-            .unwrap();
-        stdout.flush().await.unwrap();
+        let mut account = Account::new(backend, &email);
+        account.login(&password).await.unwrap();
 
-        let Some(line) = line_reader.next_line().await.unwrap() else {
-            eprintln!("Failed to read totp");
-            return;
-        };
+        if account.is_awaiting_totp() {
+            let mut stdout = tokio::io::stdout();
+            let mut line_reader = tokio::io::BufReader::new(tokio::io::stdin()).lines();
+            stdout
+                .write_all("Please Input TOTP:".as_bytes())
+                .await
+                .unwrap();
+            stdout.flush().await.unwrap();
 
-        let totp = line.trim_end_matches('\n');
-        account.submit_totp(totp).await.unwrap();
-    }
+            let Some(line) = line_reader.next_line().await.unwrap() else {
+                eprintln!("Failed to read totp");
+                return;
+            };
+
+            let totp = line.trim_end_matches('\n');
+            account.submit_totp(totp).await.unwrap();
+        }
+
+        vec![account]
+    };
 
     let (observer, task) = ObserverBuilder::new(Box::new(StdOutNotifier {}))
         .poll_interval(Duration::from_secs(10))
         .build();
+
+    println!("Starting observer...");
     let h = tokio::spawn(task);
 
-    observer.add_account(account).await.unwrap();
+    for a in accounts {
+        observer.add_account(a).await.unwrap();
+    }
+
     tokio::signal::ctrl_c().await.unwrap();
+
+    println!("Saving observer state");
+    let config = observer.generate_config(encryption_key).await.unwrap();
+    write_config_file(&config).await;
 
     h.abort();
     let _ = h.await;
+    println!("Goodbye");
+}
+
+async fn load_config(
+    key: &EncryptionKey,
+    backends: &[Arc<dyn Backend>],
+) -> Option<Vec<ConfigAccount>> {
+    if let Some(bytes) = load_config_file().await {
+        return Some(Config::load(key, &backends, &bytes).unwrap());
+    }
+    None
+}
+
+fn get_or_create_encryption_key() -> Secret<EncryptionKey> {
+    let entry = keyring::Entry::new("you-have-mail-common", "secret-key").unwrap();
+    match entry.get_password() {
+        Err(e) => {
+            if !matches!(e, keyring::Error::NoEntry) {
+                panic!("failed to load encryption key: {e}")
+            }
+
+            let key = EncryptionKey::new();
+            entry
+                .set_password(&hex::encode(key.expose_secret()))
+                .unwrap();
+            return key;
+        }
+        Ok(s) => {
+            let bytes = hex::decode(s).unwrap();
+            let fixed: [u8; 32] = bytes.try_into().unwrap();
+            Secret::new(EncryptionKey::from(fixed))
+        }
+    }
+}
+
+fn get_config_file_dir() -> PathBuf {
+    dirs::config_dir().unwrap()
+}
+
+const CONFIG_FILE_NAME: &str = "you-have-mail-common-cli.conf";
+
+fn get_config_file_path() -> PathBuf {
+    dirs::config_dir().unwrap().join(CONFIG_FILE_NAME)
+}
+async fn load_config_file() -> Option<Vec<u8>> {
+    match tokio::fs::read(get_config_file_path()).await {
+        Ok(v) => Some(v),
+        Err(_) => None,
+    }
+}
+
+async fn write_config_file(data: &[u8]) {
+    let config_path = get_config_file_dir();
+    tokio::fs::create_dir_all(&config_path).await.unwrap();
+    let config_file = config_path.join(CONFIG_FILE_NAME);
+    tokio::fs::write(config_file, data).await.unwrap();
 }
