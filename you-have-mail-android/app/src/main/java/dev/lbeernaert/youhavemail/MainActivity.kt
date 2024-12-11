761d38bcd54b130f02db4ba9ea3cd7dd2e13975c
pangratt <pangratt@proton.me>
2024-12-11 19:17:36 +0100

    chore: implemented todo related to displaying error dialog

--- you-have-mail-android/app/src/main/java/dev/lbeernaert/youhavemail/MainActivity.kt
+++ you-have-mail-android/app/src/main/java/dev/lbeernaert/youhavemail/MainActivity.kt
@@ -16,12 +16,16 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -35,7 +39,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.startActivity
 import dev.lbeernaert.youhavemail.app.NotificationActionClicked
@@ -51,6 +57,7 @@ import dev.lbeernaert.youhavemail.app.getLogPath
 import dev.lbeernaert.youhavemail.app.oneshotWorker
 import dev.lbeernaert.youhavemail.screens.MainNavController
 import dev.lbeernaert.youhavemail.ui.theme.YouHaveMailTheme
+import kotlin.system.exitProcess
 
 
 const val activityLogTag = "activity"
@@ -108,7 +115,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
                     val permissionOpenDialog = remember { mutableStateOf(false) }
                     val rationalPermissionOpenDialog = remember { mutableStateOf(false) }
 
@@ -155,7 +161,7 @@ class MainActivity : ComponentActivity() {
                     }
 
                     if (mState == null) {
-                        TODO("display text saying service failed to initiliaze")
+                        ShowErrorDialog()
                     } else {
                         MainNavController(
                             context = this,
@@ -308,8 +314,47 @@ fun ShowSettingDialog(context: Context, openDialog: MutableState<Boolean>) {
                         Text(stringResource(id = R.string.ok))
                     }
                 }
-
             },
         )
     }
+}
+
+@Composable
+private fun ShowErrorDialog() {
+    AlertDialog(
+        onDismissRequest = {},
+        title = {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                fontSize = 20.sp,
+                text = "Error"
+            )
+        },
+        text = {
+            Column() {
+                Text(
+                    stringResource(id = R.string.service_uninitialized),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        },
+        buttons = {
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(bottom = 20.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    modifier = Modifier.wrapContentWidth(),
+                    onClick = {
+                        exitProcess(0)
+                    }
+                ) {
+                    Text("Exit")
+                }
+            }
+        }
+    )
 }
